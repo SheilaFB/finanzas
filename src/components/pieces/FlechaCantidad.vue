d19b87869a22a6bca6deef8079c76eea9b408c63
Sheila <marta@empresa.com>
2024-06-04 16:24:54 +0200

    Layout terminado Añadida cantidad de la cuenta a la  vista y componente para ingresos y gastos

---
+++ src/components/pieces/FlechaCantidad.vue
@@ -0,0 +1,56 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+  />
+  <div class="arrow">
+    <span class="material-symbols-outlined" :class="{ arrowRed: !isPositive }">
+      {{ arrowDirection }}
+    </span>
+    <span>{{ amount }}</span>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  name: "ArrowAmount",
+  props: {
+    isPositive: {
+      type: Boolean,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  setup(props) {
+    const arrowDirection = computed(() =>
+      props.isPositive ? "arrow_upward" : "arrow_downward"
+    );
+
+    return {
+      arrowDirection,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.arrow {
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+
+  .material-symbols-outlined {
+    color: #6aa34d;
+    margin-right: 0.5rem;
+  }
+
+  .arrowRed {
+    color: #e74c3c;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,80 @@
 <template>
-<Layout>
-  <div class="home">
-  <p>Hola mundo</p>
-  </div>
-</Layout>
+  <Layout>
+    <div class="home">
+      <div class="cuenta">
+        <p>Mi cuenta:</p>
+        <p>
+          {{ cantidadCuenta }}<span class="moneda">{{ moneda }}</span>
+        </p>
+      </div>
+      <div class="ingresoGasto">
+        <FlechaCantidadVue :isPositive="true" :amount="100" />
+        <FlechaCantidadVue :isPositive="false" :amount="100" />
+      </div>
+    </div>
+  </Layout>
 </template>
 
 <script>
-import Layout from '@/components/Layout.vue'
+import Layout from "@/components/Layout.vue";
+import { ref } from "vue";
+import FlechaCantidadVue from "@/components/pieces/FlechaCantidad.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
     Layout,
+    FlechaCantidadVue,
+  },
+
+  setup() {
+    let cantidadCuenta = ref(4000);
+    let moneda = ref("€");
+
+    return {
+      cantidadCuenta,
+      moneda,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  width: 100%;
+  p {
+    margin: 0;
+  }
+  .cuenta {
+    width: 15rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1rem auto;
+    padding: 1rem 0;
+    --s: 50px;
+    --t: 5px;
+    --g: 20px;
+
+    padding: calc(var(--g) + var(--t));
+    outline: var(--t) solid #afa08c;
+    outline-offset: calc(-1 * var(--t));
+    mask: conic-gradient(at var(--s) var(--s), #0000 75%, #000 0) 0 0 /
+        calc(100% - var(--s)) calc(100% - var(--s)),
+      linear-gradient(#000 0 0) content-box;
+
+    p:first-child {
+      color: #808f82;
+    }
+
+    p:nth-child(2) {
+      font-size: 2rem;
+
+      .moneda {
+        margin-left: 0.2rem;
+        color: #ed9b40;
+      }
+    }
   }
 }
-</script>
+</style>
